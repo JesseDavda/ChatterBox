bfb01e9cd6d97939f2e1bfbd5c818ff5c9e47f1d
Jesse Davda <stratosphere1243@gmail.com>
2015-04-26 22:12:28 +0100

    adding the index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var currentPage = "index.html";
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/" + currentPage);
+});
+
+io.on("connection", function(socket) {
+	console.log("a user connnected");
+	socket.on("disconnect", function() {
+		console.log("user disconnected");
+	});
+});
+
+io.on("connection", function(socket) {
+	socket.on("chat message", function(msg) {
+		console.log("message " + msg);
+	});
+});
+
+io.on("connection", function(socket) {
+	socket.on("chat message", function(msg) {
+		io.emit("chat message", msg);
+	});
+});
+
+http.listen(3000, function() {
+	console.log("chat listening on port 3000");
+});
