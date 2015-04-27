862a5782da5cc7356ba6456d342ff67ee97b86b2
OrganTrouble <jessepilot@live.com>
2015-04-27 16:44:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ io.on("connection", function(socket) {
 	socket.on("disconnect", function() {
 		console.log("user disconnected");
 	});
-});
-
-io.on("connection", function(socket) {
+	
 	socket.on("chat message", function(msg) {
 		console.log("message " + msg);
 		io.emit("chat message", msg);
