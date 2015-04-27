fc7feba0c64e51ecc09467764a9ccda8daba41ff
OrganTrouble <jessepilot@live.com>
2015-04-27 16:44:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,10 @@ io.on("connection", function(socket) {
 io.on("connection", function(socket) {
 	socket.on("chat message", function(msg) {
 		console.log("message " + msg);
-	});
-});
-
-io.on("connection", function(socket) {
-	socket.on("chat message", function(msg) {
 		io.emit("chat message", msg);
 	});
 });
+
 
 http.listen(80, function() {
 	console.log("chat listening on port 3000");
